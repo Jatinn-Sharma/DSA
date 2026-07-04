28eaeb10c278382987fcfe7e9b507fab273ccabe
Jatin <jatinsharma230sharma@gmail.com>
2026-07-04 08:29:39 +0530

    Time: 0 ms (100.00%) | Memory: 14.1 MB (38.19%) - LeetSync

--- 40-combination-sum-ii/combination-sum-ii.cpp
+++ 40-combination-sum-ii/combination-sum-ii.cpp
@@ -1,4 +1,13 @@
 class Solution {
+public:
+    vector<vector<int>> combinationSum2(vector<int>& nums, int target) {
+        sort(nums.begin(),nums.end());
+        vector<int>temp;
+        vector<vector<int>>ans;
+        res(nums,temp,ans,target,0);
+        return ans;
+    }
+    private:
     void res(vector<int>&nums,vector<int>&temp,vector<vector<int>>&ans,int target,int idx){
         if(target==0){
             ans.push_back(temp);
@@ -7,20 +16,13 @@ class Solution {
         if(target<0 || idx==nums.size()){
             return;
         }
-        temp.push_back(nums[idx]);
-        res(nums,temp,ans,target-nums[idx],idx+1);
-        temp.pop_back();
-        while(idx<nums.size()-1 && nums[idx]==nums[idx+1]){
-            idx++;
+        for(int i=idx;i<nums.size();i++){
+            if(i>idx && nums[i]==nums[i-1]){
+                continue;
+            }
+            temp.push_back(nums[i]);
+            res(nums,temp,ans,target-nums[i],i+1);
+            temp.pop_back();
         }
-        res(nums,temp,ans,target,idx+1);
-    }
-public:
-    vector<vector<int>> combinationSum2(vector<int>& nums, int target) {
-        sort(nums.begin(),nums.end());
-        vector<int>temp;
-        vector<vector<int>>ans;
-        res(nums,temp,ans,target,0);
-        return ans;
     }
 };
