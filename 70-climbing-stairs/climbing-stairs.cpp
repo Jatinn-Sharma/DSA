1a7b82e15a94dc5ca218dae4ff9b4a946ce328bc
Jatin <jatinsharma230sharma@gmail.com>
2026-07-08 17:24:26 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (72.91%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -3,7 +3,7 @@ public:
     int climbStairs(int n) {
         int a=1,b=1;
         for(int i=2;i<=n;i++){
-            int c=a+b;//next step
+            int c=a+b;
             a=b;
             b=c;
         }
