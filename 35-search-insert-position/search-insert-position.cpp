60fab09ba13ee931946c199541e36dc605d8bd07
Jatin <jatinsharma230sharma@gmail.com>
2026-07-10 16:33:54 +0530

    Time: 0 ms (100.00%) | Memory: 13.7 MB (43.13%) - LeetSync

--- 35-search-insert-position/search-insert-position.cpp
+++ 35-search-insert-position/search-insert-position.cpp
@@ -1,22 +1,25 @@
 class Solution {
-public:
-    int searchInsert(vector<int>& nums, int target) {
-        int lo=0,hi=nums.size()-1,idx=-1;
+    int lb(vector<int>&nums,int target,int lo,int hi){
+        int ans=0;
         while(lo<=hi){
             int mid=lo+(hi-lo)/2;
             if(nums[mid]==target){
-                idx=mid;
-                return idx;
+                ans=mid;
+                return ans;
             }
             else if(nums[mid]<target){
-                idx=mid+1;
+                ans=mid+1;
                 lo=mid+1;
             }
             else {
-                idx=mid;
+                ans=mid;
                 hi=mid-1;
             }
         }
-        return idx;
+        return ans;
+    }
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        return lb(nums,target,0,nums.size()-1);
     }
 };
