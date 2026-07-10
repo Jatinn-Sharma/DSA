0d2b9d205b7ea12196eb94b3c311bd75d53c924b
Jatin <jatinsharma230sharma@gmail.com>
2026-07-10 16:11:22 +0530

    Time: 0 ms (100.00%) | Memory: 17.6 MB (54.20%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,48 +1,38 @@
 class Solution {
-public:
-int firstoc(vector<int>&nums,int target){
-    int lo=0,hi=nums.size()-1;
-    while(lo<=hi){
-        int mid=lo+(hi-lo)/2;
-        if(nums[mid]==target){
-            if(mid>0 && nums[mid]==nums[mid-1]){
+    int firstocc(vector<int>&nums,int target,int lo,int hi){
+        while(lo<=hi){
+            int mid=lo+(hi-lo)/2;
+            if(nums[mid]==target){
+                if(mid>0 && nums[mid]==nums[mid-1]){
+                    hi=mid-1;
+                }
+                else return mid;
+            }
+            else if(nums[mid]>target){
                 hi=mid-1;
             }
-            else{
-                return mid;
-            }
+            else lo=mid+1;
         }
-        else if(nums[mid]>target){
-            hi=mid-1;
-        }
-        else{
-            lo=mid+1;
-        }
+        return -1;
     }
-    return -1;
-}
-int lastoc(vector<int>&nums,int target){
-    int lo=0,hi=nums.size()-1;
-    while(lo<=hi){
-        int mid=lo+(hi-lo)/2;
-        if(nums[mid]==target){
-            if(mid<nums.size()-1 && nums[mid]==nums[mid+1]){
-                lo=mid+1;
+    int lastocc(vector<int>&nums,int target,int lo,int hi){
+        while(lo<=hi){
+            int mid=lo+(hi-lo)/2;
+            if(nums[mid]==target){
+                if(mid<hi && nums[mid]==nums[mid+1]){
+                    lo=mid+1;
+                }
+                else return mid;
             }
-            else{
-                return mid;
+            else if(nums[mid]>target){
+                hi=mid-1;
             }
+            else lo=mid+1;
         }
-        else if(nums[mid]>target){
-            hi=mid-1;
-        }
-        else{
-            lo=mid+1;
-        }
+        return -1;
     }
-    return -1;
-}
+public:
     vector<int> searchRange(vector<int>& nums, int target) {
-        return {firstoc(nums,target),lastoc(nums,target)};
+        return {firstocc(nums,target,0,nums.size()-1),lastocc(nums,target,0,nums.size()-1)};
     }
 };
