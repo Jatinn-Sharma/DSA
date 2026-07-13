bae0042ca2ea9e6749bd1e112d4e75981d547ef2
Jatin <jatinsharma230sharma@gmail.com>
2026-07-13 12:08:04 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (48.96%) - LeetSync

--- 1212-sequential-digits/sequential-digits.cpp
+++ 1212-sequential-digits/sequential-digits.cpp
@@ -4,7 +4,7 @@ public:
         vector<int> ans;
         string digits = "123456789";
         // Generate sequential numbers of different lengths
-        for (int len = 2; len <= 9; ++len) {
+        for (int len = 2; len <= 9; len++) {
             for (int start = 0; start + len <= 9; start++) {
                 int num = stoi(digits.substr(start, len));
                 if (num >= low && num <= high) {
