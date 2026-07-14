238f0aaf3c074c5de9fadb0ea95732d3c4e44f3b
Jatin <jatinsharma230sharma@gmail.com>
2026-07-14 12:29:49 +0530

    Time: 0 ms (100.00%) | Memory: 10.5 MB (71.46%) - LeetSync

--- 46-permutations/permutations.cpp
+++ 46-permutations/permutations.cpp
@@ -1,19 +1,19 @@
 class Solution {
-    void nop(vector<int>&nums,vector<vector<int>>&ans,int idx){
-        if(idx==nums.size()-1){
-            ans.push_back(nums);
-            return;
-        }
-        for(int i=idx;i<nums.size();i++){
-            swap(nums[i],nums[idx]);
-            nop(nums,ans,idx+1);
-            swap(nums[i],nums[idx]);
-        }
-    }
 public:
     vector<vector<int>> permute(vector<int>& nums) {
         vector<vector<int>>ans;
-        nop(nums,ans,0);
+        nop(nums,ans,0,nums.size());
         return ans;
+    }
+    private:
+    void nop(vector<int>&nums,vector<vector<int>>&ans,int idx,int n){
+        if(idx==n-1){
+            ans.push_back(nums);
+        }
+        for(int i=idx;i<n;i++){
+            swap(nums[i],nums[idx]);
+            nop(nums,ans,idx+1,n);
+            swap(nums[i],nums[idx]);
+        }
     }
 };
