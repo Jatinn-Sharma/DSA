4f4881964071b1a5a4c37a3bf40dd0bf7245c3b7
Jatin <jatinsharma230sharma@gmail.com>
2026-09-20 08:07:27 +0530

    Time: 4 ms (30.57%) | Memory: 10.7 MB (16.47%) - LeetSync

--- 9-palindrome-number/palindrome-number.cpp
+++ 9-palindrome-number/palindrome-number.cpp
@@ -1,11 +1,26 @@
+// class Solution {
+// public:
+//     bool isPalindrome(int x) {
+//         string s = to_string(x);
+//         for(int i=0;i<s.length();i++){
+//             if(s[i]!=s[s.length()-1-i]){
+//                 return false;
+//             }
+//         }
+//         return true;
+//     }
+// };
 class Solution {
 public:
     bool isPalindrome(int x) {
         string s = to_string(x);
-        for(int i=0;i<s.length();i++){
-            if(s[i]!=s[s.length()-1-i]){
+        int i=0,j=s.length()-1;
+        while(i<j){
+            if(s[i]!=s[j]){
                 return false;
             }
+            i++;
+            j--;
         }
         return true;
     }
